b45ac033ad3637375dcff53cbd462db285d18825
Nitrammm78999 <166219034+Nitrammm78999@users.noreply.github.com>
2024-04-16 17:31:19 +0200

    Add files via upload

---
+++ CODE/test.js
@@ -0,0 +1,7 @@
+const ageMartin = 16;
+const yearMartin = 2007;
+const ageJeanne = 14;
+const yearJeanne = 2010;
+const yearD = yearMartin - yearJeanne;
+
+console.log(`Différence age entre Martin et Jeanne`);
